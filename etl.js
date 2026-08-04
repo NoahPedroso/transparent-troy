fca6277628dd7a0f9c8b4a50a35bbc160643b679
Noah Pedroso <n.pedroso@me.com>
2026-08-04 18:02:04 -0400

    REFACTOR: Move ETL logic to separate script
    A lot of the work for this app so far involves taking raw geoJSON data from Rensselaer County
    and simply morphing it into a way that serves my needs. e.g. Merging distinct election district
    polygons into just a few city council district polygons, or just stripping irrelevant data.
    A lot of this also can be done beforehand, instead of on-the-fly for each user. In the future,
    it would make sense to take this separated out code and put it in a script that runs on a backend,
    and only the output (exports) would be used by the browser.
    This keeps mapping and interaction logic clean and separate, preventing a mega-file.

---
+++ etl.js
@@ -0,0 +1,117 @@
+/**
+ * etl.js
+ * 
+ * 
+ * This file handles all the preprocessing of the raw geoJSON data
+ * None of this needs to happen on the fly and is the same for all users
+ * ETL = Extract, Transform, Load (this is mostly "Transform")
+ */
+
+// Constants
+const DATA_FILENAME = 'data/troy_eds.geojson';
+
+// Types
+/**
+ * @import {GeoJson, ElectionDistrict} from "./types/geojson.js"
+ */
+
+/**
+ * Groups the rows from the ED data by which city countil district they are in
+ * @param {Array<Object>} electionDistricts the rows in the ED data
+ */
+function groupByCouncilDistrict(electionDistricts) {
+    return Object.groupBy(electionDistricts, (ed) => ed.councilDistrict);
+}
+
+
+/**
+ * Return only the relevant data to us from the raw JSON file from the county.
+ * Could be an easy one liner, but splitting into multiple lines allows good logging for understanding and debugging
+ *  Hence we will keep it in a function
+ * @param {GeoJson} rawData
+ * @returns {ElectionDistrict[]}
+ */
+function transformFeatures(rawData) {
+    console.log("FEATURE TRANSFORMATION STEP 0: Raw Data:", rawData);
+
+    const fullFeatures = rawData.features.filter(potentialFeature => potentialFeature.type == "Feature");
+    console.log("FEATURE TRANSFORMATION STEP 1: Only Verified Features", fullFeatures);
+
+    const transformedFeatures = fullFeatures.map(feature => ({
+        edId: feature.properties.ED,
+        councilDistrict: feature.properties.CNCLDIST,
+        coordinates: feature.geometry.coordinates,
+    }));
+    console.log("FEATURE TRANSFORMATION STEP 2: Only Desired Attributes", transformedFeatures);
+    
+    return transformedFeatures;
+
+    // One liner just for demo (never called)
+    return rawData.features.filter(potentialFeature => potentialFeature.type = "Feature")
+                           .map(feature => ({
+                                edId: feature.properties.ED,
+                                councilDistrict: feature.properties.CNCLDIST,
+                                coordinates: feature.geometry.coordinates,
+                            }));
+}
+
+/**
+ * DATA MANIPULATION
+ */
+
+// Import the geojson data
+const rawData = await fetch(DATA_FILENAME).then(res=>res.json());
+console.log("Raw imported data:", rawData);
+
+// Grab only the features, and relevant data
+const electionDistricts = transformFeatures(rawData);
+console.log("Election Districts:", electionDistricts);
+
+// Create groups of EDs by CD
+const groupedDistricts = groupByCouncilDistrict(electionDistricts);
+console.log("Grouped Districts:", groupedDistricts);
+
+
+/* Use turf.js to create unified polygons of the borders of groups of EDs. 
+    NOTE: https://github.com/w8r/martinez may be better here (smaller) */
+let councilDistrictFeatures = turf.featureCollection([]);
+Object.entries(groupedDistricts).forEach(([key, val]) => {
+    // Assuming that the key looks like "NTH", we extract the N 
+    // TODO: Add verification that the data looks like this
+    const councilDistrictNumber = key[0];
+    councilDistrictFeatures.features.push(turf.union(
+        turf.featureCollection(val.map(ed => turf.polygon(ed.coordinates))),
+        {
+            // Keep council district in feature properties. Any other useful info is tied to council district number
+            properties: {
+                district: Number(councilDistrictNumber)
+            }
+        })
+    );
+});
+console.log("Unioned features:", councilDistrictFeatures);
+
+// Get full troy polygon
+
+
+
+/**********************************
+ *            EXPORTS             *
+ **********************************/
+// Polygon Features
+export {councilDistrictFeatures};
+export const TROY_POLYGON = turf.union(councilDistrictFeatures);
+// Point Features
+export const TROY_CENTER = turf.center(TROY_POLYGON); 
+/* Could be helpful to create quicklinks (e.g. transparenttroy.com/?d=4)
+    that start with that district centered */
+export const DISTRICT_CENTERS = councilDistrictFeatures.features.sort((a, b) => a.properties.district - b.properties.district)
+                                                        .map(feature => turf.center(feature));
+/* Unsure if it is ideal to use these instead of just the array indices or an object with keys. 
+    Commenting out for now because I think it is a bad idea actually. */
+// export const DISTRICT_1_CENTER = districtCenters[0];
+// export const DISTRICT_2_CENTER = districtCenters[1];
+// export const DISTRICT_3_CENTER = districtCenters[2];
+// export const DISTRICT_4_CENTER = districtCenters[3];
+// export const DISTRICT_5_CENTER = districtCenters[4];
+// export const DISTRICT_6_CENTER = districtCenters[5];
--- main.js
+++ main.js
@@ -5,110 +5,20 @@
  * All other scripts should be auxiliary to this.
  */
 
-
-// Constants
-const DATA_FILENAME = 'data/troy_eds.geojson';
-// TODO: Calculate the "true" center based on coordinates
-const TROY_CENTER = Object.freeze({
-    latitude:   42.731259,
-    longitute: -73.691695
-});
-/* TODO: Calculate the "true" centers of each district.
-    Could be helpful to create quicklinks 
-    (e.g. transparenttroy.com/?d=4) that start with that district centered */  
-// Types
-/**
- * @import {GeoJson, ElectionDistrict} from "./types/geojson.js"
- */
-
-/**
- * Groups the rows from the ED data by which city countil district they are in
- * @param {Array<Object>} electionDistricts the rows in the ED data
- */
-function groupByCouncilDistrict(electionDistricts) {
-    return Object.groupBy(electionDistricts, (ed) => ed.councilDistrict);
-}
-
-
-/**
- * Return only the relevant data to us from the raw JSON file from the county.
- * Could be an easy one liner, but splitting into multiple lines allows good logging for understanding and debugging
- *  Hence we will keep it in a function
- * @param {GeoJson} rawData
- * @returns {ElectionDistrict[]}
- */
-function transformFeatures(rawData) {
-    console.log("FEATURE TRANSFORMATION STEP 0: Raw Data:", rawData);
-
-    const fullFeatures = rawData.features.filter(potentialFeature => potentialFeature.type == "Feature");
-    console.log("FEATURE TRANSFORMATION STEP 1: Only Verified Features", fullFeatures);
-
-    const transformedFeatures = fullFeatures.map(feature => ({
-        edId: feature.properties.ED,
-        councilDistrict: feature.properties.CNCLDIST,
-        coordinates: feature.geometry.coordinates,
-    }));
-    console.log("FEATURE TRANSFORMATION STEP 2: Only Desired Attributes", transformedFeatures);
-    
-    return transformedFeatures;
-
-    // One liner just for demo (never called)
-    return rawData.features.filter(potentialFeature => potentialFeature.type = "Feature")
-                           .map(feature => ({
-                                edId: feature.properties.ED,
-                                councilDistrict: feature.properties.CNCLDIST,
-                                coordinates: feature.geometry.coordinates,
-                            }));
-}
-
-/**
- * DATA MANIPULATION
- */
-
-// Import the geojson data
-const rawData = await fetch(DATA_FILENAME).then(res=>res.json());
-console.log("Raw imported data:", rawData);
-
-// Grab only the features, and relevant data
-const electionDistricts = transformFeatures(rawData);
-console.log("Election Districts:", electionDistricts);
-
-// Create groups of EDs by CD
-const groupedDistricts = groupByCouncilDistrict(electionDistricts);
-console.log("Grouped Districts:", groupedDistricts);
-
-
-/* Use turf.js to create unified polygons of the borders of groups of EDs. 
-    NOTE: https://github.com/w8r/martinez may be better here (smaller) */
-const councilDistrictFeatures = turf.featureCollection([]);
-Object.entries(groupedDistricts).forEach(([key, val]) => {
-    // Assuming that the key looks like "NTH", we extract the N 
-    // TODO: Add verification that the data looks like this
-    const councilDistrictNumber = key[0];
-    councilDistrictFeatures.features.push(turf.union(
-        turf.featureCollection(val.map(ed => turf.polygon(ed.coordinates))),
-        {
-            // Keep council district in feature properties. Any other useful info is tied to council district number
-            properties: {
-                district: Number(councilDistrictNumber)
-            }
-        })
-    );
-});
-console.log("Unioned features:", councilDistrictFeatures);
-
+// Imports
+import { TROY_CENTER, TROY_POLYGON, councilDistrictFeatures } from "./etl.js";
 /**
  * MAPPING
  */
 
 // Display map
-const zoomLevel = 15
-var map = L.map('map').setView([TROY_CENTER.latitude, TROY_CENTER.longitute], zoomLevel);
+const zoomLevel = 12
+console.log(TROY_CENTER.geometry.coordinates);
+var map = L.map('map').setView(TROY_CENTER.geometry.coordinates.toReversed(), zoomLevel);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
 // Draw districts
 
 // TODO: this function should show the info for each city council member 
@@ -116,10 +26,19 @@ function addFeatureInfo(feature, layer) {
     layer.bindPopup(`Council District: ${feature.properties.district}`);
 }
 
+
+L.geoJSON(TROY_POLYGON, {
+    style: {
+        color: '#3e916e',
+        weight: 1,
+        fillOpacity: 0.4
+    },
+}).addTo(map);
+
 L.geoJSON(councilDistrictFeatures, {
     style: {
         color: '#d10e0e',
-        weight: 1,
+        weight: 5,
         fillOpacity: 0.4
     },
     onEachFeature: addFeatureInfo
